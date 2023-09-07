4230937946d4985e08a8dea3ce6708cb7b0ceeea
Jutawat Maneechot <dev.aikaisys@gmail.com>
2023-09-07 20:34:20 +0700

    SQLite Workshop EP.4

--- Workshops/sqlite/lib/db_provider.dart
+++ Workshops/sqlite/lib/db_provider.dart
@@ -5,7 +5,7 @@ import 'package:path/path.dart';
 import 'package:sqlite/models/product.dart';
 
 class DBProvider {
-  Database database;
+  late Database database;
 
   Future<bool> initDB() async {
     try {
@@ -42,5 +42,64 @@ class DBProvider {
     } catch (e) {
       throw Exception(e);
     }
+  }
+
+  Future<List<Product>> getProducts() async {
+    List<Map<String, dynamic>> maps = await database.query(
+      TABLE_PRODUCT,
+      columns: [COLUMN_ID, COLUMN_NAME, COLUMN_PRICE, COLUMN_STOCK],
+    );
+    // List<Map> maps = await database.rawQuery("SELECT * FROM $TABLE_PRODUCT");
+
+    if (maps.length > 0) {
+      return maps.map((p) => Product.fromMap(p)).toList();
+    }
+
+    return [];
+  }
+
+  Future<Product?> getProduct(int id) async {
+    List<Map<String, dynamic>> maps = await database.query(
+      TABLE_PRODUCT,
+      columns: [COLUMN_ID, COLUMN_NAME, COLUMN_PRICE, COLUMN_STOCK],
+      where: "$COLUMN_ID = ?",
+      whereArgs: [id],
+    );
+
+    if (maps.length > 0) {
+      return Product.fromMap(maps.first);
+    }
+
+    return null;
+  }
+
+  Future<Product> insertProduct(Product product) async {
+    product.id = await database.insert(TABLE_PRODUCT, product.toMap());
+    // product.id = await database.rawInsert("INSERT Into ....");
+
+    return product;
+  }
+
+  Future<int> updateProduct(Product product) async {
+    return await database.update(
+      TABLE_PRODUCT,
+      product.toMap(),
+      where: "$COLUMN_ID = ?",
+      whereArgs: [product.id],
+    );
+  }
+
+  Future<int> deleteProduct(int id) async {
+    return await database.delete(
+      TABLE_PRODUCT,
+      where: "$COLUMN_ID = ?",
+      whereArgs: [id],
+    );
+  }
+
+  Future<int> deleteAll() async {
+    // return await database.delete(TABLE_PRODUCT);
+    String sql = "DELETE FROM $TABLE_PRODUCT";
+    return await database.rawDelete(sql);
   }
 }
