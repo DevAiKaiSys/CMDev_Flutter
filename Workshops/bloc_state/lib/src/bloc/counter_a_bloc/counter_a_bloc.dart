ab92e26db3633798aa519ab83ae02a30ecb67ee5
Jutawat Maneechot <dev.aikaisys@gmail.com>
2023-09-08 22:31:22 +0700

    Bloc Workshop EP.5

--- Workshops/bloc_state/lib/src/bloc/counter_a_bloc/counter_a_bloc.dart
+++ Workshops/bloc_state/lib/src/bloc/counter_a_bloc/counter_a_bloc.dart
@@ -4,12 +4,17 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_a_event.dart';
+
 part 'counter_a_state.dart';
 
 class CounterABloc extends Bloc<CounterAEvent, CounterAState> {
-  CounterABloc() : super(CounterAInitial()) {
-    on<CounterAEvent>((event, emit) {
-      // TODO: implement event handler
+  CounterABloc() : super(CounterAState(count: 0)) {
+    on<CounterAEventAdd>((event, emit) {
+      emit(state.copyWith(count: state.count + 1));
+    });
+
+    on<CounterAEventReset>((event, emit) {
+      emit(state.copyWith(count: 0));
     });
   }
 }
--- Workshops/bloc_state/lib/src/bloc/counter_a_bloc/counter_a_event.dart
+++ Workshops/bloc_state/lib/src/bloc/counter_a_bloc/counter_a_event.dart
@@ -2,4 +2,12 @@ part of 'counter_a_bloc.dart';
 
 abstract class CounterAEvent extends Equatable {
   const CounterAEvent();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
+
+class CounterAEventAdd extends CounterAEvent {}
+
+class CounterAEventReset extends CounterAEvent {}
