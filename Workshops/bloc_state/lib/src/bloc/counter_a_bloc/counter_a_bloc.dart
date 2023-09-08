15adb9c35a7290bb920a47cb7edbaa984db197eb
Jutawat Maneechot <dev.aikaisys@gmail.com>
2023-09-08 22:04:26 +0700

    Bloc Workshop EP.4

---
+++ Workshops/bloc_state/lib/src/bloc/counter_a_bloc/counter_a_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_a_event.dart';
+part 'counter_a_state.dart';
+
+class CounterABloc extends Bloc<CounterAEvent, CounterAState> {
+  CounterABloc() : super(CounterAInitial()) {
+    on<CounterAEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ Workshops/bloc_state/lib/src/bloc/counter_a_bloc/counter_a_event.dart
@@ -0,0 +1,5 @@
+part of 'counter_a_bloc.dart';
+
+abstract class CounterAEvent extends Equatable {
+  const CounterAEvent();
+}
