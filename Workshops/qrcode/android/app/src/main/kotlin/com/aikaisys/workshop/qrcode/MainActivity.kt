f7666544c03792c7e74dae51e6de6b798f48a583
Jutawat Maneechot <dev.aikaisys@gmail.com>
2023-09-10 23:26:21 +0700

    QR Code Workshop EP.7

--- Workshops/qrcode/android/app/src/main/kotlin/com/aikaisys/workshop/qrcode/MainActivity.kt
+++ Workshops/qrcode/android/app/src/main/kotlin/com/aikaisys/workshop/qrcode/MainActivity.kt
@@ -4,11 +4,21 @@ import io.flutter.embedding.android.FlutterActivity
 
 import android.os.Bundle
 import android.widget.Toast
+//import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_LONG).show()
+        MethodChannel(
+            flutterEngine!!.getDartExecutor().getBinaryMessenger(),
+            "cm.share/share"
+        ).setMethodCallHandler { methodCall, result ->
+            if (methodCall.method == "shareFile") {
+                Toast.makeText(applicationContext, "this is toast message", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
