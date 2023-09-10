ba2cdb3f5791f5802330e5484fe2c005b3803f46
Jutawat Maneechot <dev.aikaisys@gmail.com>
2023-09-10 21:24:57 +0700

    QR Code Workshop EP.5

--- Workshops/qrcode/lib/generate_qrcode_page.dart
+++ Workshops/qrcode/lib/generate_qrcode_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class GenerateQRCodePage extends StatefulWidget {
   GenerateQRCodePage({super.key, required this.title});
@@ -10,9 +11,22 @@ class GenerateQRCodePage extends StatefulWidget {
 }
 
 class _GenerateQRCodePageState extends State<GenerateQRCodePage> {
+  final TextEditingController _textController = TextEditingController();
+
+  String _dataQRCode = "";
+
+  @override
+  void initState() {
+    _textController.addListener(() {
+      Change();
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
         actions: [
@@ -22,8 +36,34 @@ class _GenerateQRCodePageState extends State<GenerateQRCodePage> {
           )
         ],
       ),
+      body: _buildContent(),
     );
   }
 
   void shared() {}
+
+  _buildContent() => Padding(
+        padding: EdgeInsets.only(left: 30, right: 30, top: 40),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                  hintText: "Enter the text to create the qe code"),
+            ),
+            SizedBox(height: 40),
+            QrImageView(
+              backgroundColor: Colors.white,
+              data: _dataQRCode,
+              size: 150,
+            ),
+          ],
+        ),
+      );
+
+  Change() {
+    setState(() {
+      _dataQRCode = _textController.text;
+    });
+  }
 }
