718d70274a1b7a98a9d2301a881ce4619f4ed44a
Jutawat Maneechot <dev.aikaisys@gmail.com>
2023-09-11 22:33:13 +0700

    Stock Workshop 2.Login Page: Design a login form part 2

--- Workshops/stock/lib/src/pages/login/widgets/login_form.dart
+++ Workshops/stock/lib/src/pages/login/widgets/login_form.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:stock/src/config/theme.dart' as custom_theme;
 
 class LoginForm extends StatelessWidget {
   const LoginForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(22.0),
-        child: FormInput(),
+    return Stack(
+      children: [
+        _buildForm(),
+        _buildSubmitFormButton(),
+      ],
+    );
+  }
+
+  Card _buildForm() => Card(
+        margin: EdgeInsets.only(left: 22.0, right: 22.0, bottom: 22.0),
+        elevation: 2.0, // shadow
+        // ขอบโค้ง
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(22.0),
+          child: FormInput(),
+        ),
+      );
+
+  Container _buildSubmitFormButton() => Container(
+        width: 220,
+        height: 50,
+        decoration: _boxDecoration(),
+      );
+
+  BoxDecoration _boxDecoration() {
+    final gradientStart = custom_theme.Theme.gradientStart;
+    final gradientEnd = custom_theme.Theme.gradientEnd;
+
+    final boxShadowItem = (Color color) => BoxShadow(
+          color: color,
+          offset: Offset(1.0, 6.0),
+          blurRadius: 20.0,
+        );
+
+    return BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(5.0)), // ขอบโค้ง
+      boxShadow: [
+        // BoxShadow(
+        //   color: gradientStart,
+        //   offset: Offset(1.0, 6.0),
+        //   blurRadius: 20.0,
+        // ),
+        // BoxShadow(
+        //   color: gradientEnd,
+        //   offset: Offset(1.0, 6.0),
+        //   blurRadius: 20.0,
+        // ),
+        boxShadowItem(gradientStart),
+        boxShadowItem(gradientEnd),
+      ],
+      gradient: LinearGradient(
+        colors: [gradientEnd, gradientStart],
+        begin: FractionalOffset(0.0, 0.0),
+        end: FractionalOffset(1.0, 1.0),
+        stops: [0.0, 1.0],
       ),
     );
   }
