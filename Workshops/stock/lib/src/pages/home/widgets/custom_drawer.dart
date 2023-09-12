ef5c9ef3a8b3e8c655170389dfd2ac09a5b05fc8
Jutawat Maneechot <dev.aikaisys@gmail.com>
2023-09-13 00:12:37 +0700

    Stock Workshop 2.Login Page: Login and Logout

--- Workshops/stock/lib/src/pages/home/widgets/custom_drawer.dart
+++ Workshops/stock/lib/src/pages/home/widgets/custom_drawer.dart
@@ -1,21 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stock/src/constants/setting.dart';
+import 'package:stock/src/config/route.dart' as custom_route;
 
-class CustomDrawer extends StatelessWidget {
+class CustomDrawer extends StatefulWidget {
   const CustomDrawer({super.key});
 
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: [
-          TextButton(
-            onPressed: () {
-              //todo
-            },
-            child: Text('logout'),
+          Spacer(),
+          // TextButton(
+          //   onPressed: () {
+          //     //todo
+          //   },
+          //   child: Text('logout'),
+          // )
+          ListTile(
+            leading: FaIcon(
+              // FontAwesomeIcons.signOutAlt,
+              FontAwesomeIcons.rightFromBracket,
+              color: Colors.grey,
+            ),
+            // trailing: FaIcon(
+            //   FontAwesomeIcons.rightFromBracket,
+            //   color: Colors.grey,
+            // ),
+            title: Text('Logout'),
+            onTap: showDialogLogout,
           )
         ],
       ),
+    );
+  }
+
+  void showDialogLogout() {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true, // กดข้างนอกแล้วปิด dialog
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          content: Text('Are you sure you want to log out?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.red,
+              ),
+              child: Text('Logout'),
+              onPressed: () {
+                SharedPreferences.getInstance().then((prefs) {
+                  prefs.remove(Setting.TOKEN_PREF);
+                  // prefs.clear();
+                  Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                  Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    custom_route.Route.login,
+                    (route) => false,
+                  );
+                });
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
