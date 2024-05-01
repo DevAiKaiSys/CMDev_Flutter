8f394743641f31c7c1328a5f7069b99477f6846b
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-05-01 18:36:16 +0700

    Flutter Online 2024 Ep.29 textfield widget

--- Workshops/flutter_application/lib/screens/home_screen.dart
+++ Workshops/flutter_application/lib/screens/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:flutter_application/lesson/ep24/my_column_widget.dart';
 import 'package:flutter_application/lesson/ep26/my_sizedbox.dart';
 import 'package:flutter_application/lesson/ep27/my_icon_widget.dart';
 import 'package:flutter_application/lesson/ep28/my_icon_button.dart';
+import 'package:flutter_application/lesson/ep29/my_text_field.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -45,6 +46,7 @@ class HomeScreen extends StatelessWidget {
             MySizedBoxWidget(),
             MyIconWidget(),
             MyIconButtonWidget(),
+            MyTextFieldWidget(),
           ],
         ),
       ),
