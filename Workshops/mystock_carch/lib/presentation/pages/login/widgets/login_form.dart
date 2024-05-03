dded915b7379bb9be07151c60599696dbe7d7c06
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-05-04 00:06:45 +0700

    keyboardType & action icon & next Focus

--- Workshops/mystock_carch/lib/presentation/pages/login/widgets/login_form.dart
+++ Workshops/mystock_carch/lib/presentation/pages/login/widgets/login_form.dart
@@ -69,38 +69,7 @@ class _LoginFormState extends State<LoginForm> {
         height: 50,
         decoration: _boxDecoration(),
         child: TextButton(
-          onPressed: () {
-            String username = usernameController.text;
-            String password = passwordController.text;
-
-            _errorUsername = null;
-            _errorPassword = null;
-
-            if (!EmailSubmitRegexValidator().isValid(username)) {
-              _errorUsername = 'The Email mute be a valid email.';
-            }
-
-            if (password.length < 6) {
-              _errorPassword = 'Must be at least 6 characters.';
-            }
-
-            if (_errorUsername == null && _errorPassword == null) {
-              // debugPrint('form valid');
-              showLoading();
-              Future.delayed(const Duration(seconds: 2)).then((value) async {
-                Navigator.pop(context);
-                if (username == 'aikaisys@dev.com' && password == '1234567') {
-                  debugPrint('login successfully');
-                } else {
-                  showAlertBar();
-                }
-                setState(() {});
-              });
-            } else {
-              debugPrint('form invalid');
-              setState(() {});
-            }
-          },
+          onPressed: _onLogin,
           child: const Text(
             'LOGIN',
             style: TextStyle(
@@ -133,6 +102,39 @@ class _LoginFormState extends State<LoginForm> {
       flushbarPosition: FlushbarPosition.TOP,
       flushbarStyle: FlushbarStyle.GROUNDED,
     ).show(context);
+  }
+
+  void _onLogin() {
+    String username = usernameController.text;
+    String password = passwordController.text;
+
+    _errorUsername = null;
+    _errorPassword = null;
+
+    if (!EmailSubmitRegexValidator().isValid(username)) {
+      _errorUsername = 'The Email mute be a valid email.';
+    }
+
+    if (password.length < 6) {
+      _errorPassword = 'Must be at least 6 characters.';
+    }
+
+    if (_errorUsername == null && _errorPassword == null) {
+      // debugPrint('form valid');
+      showLoading();
+      Future.delayed(const Duration(seconds: 2)).then((value) async {
+        Navigator.pop(context);
+        if (username == 'aikaisys@dev.com' && password == '1234567') {
+          debugPrint('login successfully');
+        } else {
+          showAlertBar();
+        }
+        setState(() {});
+      });
+    } else {
+      debugPrint('form invalid');
+      setState(() {});
+    }
   }
 
   BoxDecoration _boxDecoration() {
@@ -183,6 +185,15 @@ class FormInput extends StatefulWidget {
 class _FormInputState extends State<FormInput> {
   final _color = Colors.black54;
 
+  late bool _obscureTextPassword;
+  final _passwordFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    _obscureTextPassword = true;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -205,6 +216,7 @@ class _FormInputState extends State<FormInput> {
       );
 
   TextField _buildPassword() => TextField(
+        focusNode: _passwordFocusNode,
         controller: widget.passwordController,
         decoration: InputDecoration(
           border: InputBorder.none,
@@ -216,8 +228,22 @@ class _FormInputState extends State<FormInput> {
             color: _color,
           ),
           errorText: widget.errorPassword,
+          suffixIcon: IconButton(
+            icon: FaIcon(
+              _obscureTextPassword
+                  ? FontAwesomeIcons.eye
+                  : FontAwesomeIcons.eyeSlash,
+              color: _color,
+              size: 15.0,
+            ),
+            onPressed: () {
+              setState(() {
+                _obscureTextPassword = !_obscureTextPassword;
+              });
+            },
+          ),
         ),
-        obscureText: true,
+        obscureText: _obscureTextPassword,
       );
 
   TextField _buildUsername() => TextField(
@@ -234,5 +260,10 @@ class _FormInputState extends State<FormInput> {
           ),
           errorText: widget.errorUsername,
         ),
+        keyboardType: TextInputType.emailAddress,
+        textInputAction: TextInputAction.next,
+        onSubmitted: (String value) {
+          FocusScope.of(context).requestFocus(_passwordFocusNode);
+        },
       );
 }
